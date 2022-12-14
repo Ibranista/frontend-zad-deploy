0c8909815d585feb80d9bf79a3660d732f0d46b6
Ibranista <ibranista88@gmail.com>
2022-12-14 05:32:24 +0100

    changed the base.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/zadfm/",
+  base:"/",
   define: {
     "process.env": process.env,
   },
